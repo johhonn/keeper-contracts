1f692ab73c0d880fb7a12974e793fc511127e0eb
ssallam <samer@oceanprotocol.com>
2018-10-12 15:33:11 +0200

    Emit events in the payments conditions (so we can deploy contracts for testing).

--- contracts/service_agreement/PaymentConditions.sol
+++ contracts/service_agreement/PaymentConditions.sol
@@ -5,21 +5,24 @@ import 'ServiceAgreement.sol';
 
 contract PaymentConditions{
 
-  ServiceAgreement private serviceAgreementStorage;
-  constructor(address _serviceAgreementAddress) public {
+    ServiceAgreement private serviceAgreementStorage;
+    constructor(address _serviceAgreementAddress) public {
         require(_serviceAgreementAddress != address(0), 'invalid contract address');
         serviceAgreementStorage = ServiceAgreement(_serviceAgreementAddress);
-  }
+    }
 
-  event PaymentLocked(string _type, bytes32 serviceId, bytes32 condition, bytes32 payment);
-  event PaymentReleased(string _type, bytes32 serviceId, bytes32 condition, bytes32 payment);
+    event PaymentLocked(string _type, bytes32 serviceId, bytes32 condition, bytes32 payment);
+    event PaymentReleased(string _type, bytes32 serviceId, bytes32 condition, bytes32 payment);
 
 
-  function lockPayment(bytes32 serviceId, uint256 tokens, bytes32 assetId, bytes32 provider) public returns(bool) {
-    return true;
-  }
+    function lockPayment(bytes32 serviceId, uint256 tokens, bytes32 assetId, bytes32 provider) public returns(bool) {
+        bytes32 payment = 0;
+        emit PaymentLocked("sla", serviceId, this.lockPayment.selector, payment);
+        return true;
+    }
 
-  function releasePayment(bytes32 serviceId, bytes32 paymentId) public returns(bool) {
-    return true;
-  }
+    function releasePayment(bytes32 serviceId, bytes32 paymentId) public returns(bool) {
+        emit PaymentReleased("sla", serviceId, this.releasePayment.selector, payment);
+        return true;
+    }
 }
