759aff735ada0c8f52022aa20ae5b32810d48be7
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2018-10-12 14:42:44 +0200

    update initialized contracts in sla

--- contracts/service_agreement/PaymentConditions.sol
+++ contracts/service_agreement/PaymentConditions.sol
@@ -1,20 +1,22 @@
 pragma solidity ^0.4.25;
 
-import 'browser/ServiceAgreement.sol';
+import 'ServiceAgreement.sol';
 
 
 contract PaymentConditions{
 
-    ServiceAgreement private serviceAgreementStorage;
-    constructor(address _serviceAgreementAddress) public {
+  ServiceAgreement private serviceAgreementStorage;
+  constructor(address _serviceAgreementAddress) public {
         require(_serviceAgreementAddress != address(0), 'invalid contract address');
         serviceAgreementStorage = ServiceAgreement(_serviceAgreementAddress);
     }
 
 
-    function updateState(bytes32 serviceId, bytes32 funcFingerPrint, string eventId) private returns (bool);
+  event PaymentLocked(string _type, bytes32 serviceId, bytes32 condition, bytes32 payment);
+  event PaymentReleased(string _type, bytes32 serviceId, bytes32 condition, bytes32 payment);
 
-    function lockPayment(bytes32 serviceId, uint256 tokens, bytes32 assetId, bytes32 provider) public returns(bool);
 
-    function releasePayment(bytes32 serviceId, bytes32 paymentId) public returns(bool);
+  function lockPayment(bytes32 serviceId, uint256 tokens, bytes32 assetId, bytes32 provider) public returns(bool);
+
+  function releasePayment(bytes32 serviceId, bytes32 paymentId) public returns(bool);
 }
