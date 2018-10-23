55318ddcc4ba720e9f59b6087caa11ccbf2d37a5
Bill Barman <bill@anantya.com>
2018-10-23 10:25:11 +0800

    change to valueType, identies to didRegister

--- contracts/didregistry/DIDRegistry.sol
+++ contracts/didregistry/DIDRegistry.sol
@@ -3,34 +3,38 @@ pragma solidity 0.4.25;
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract DIDRegistry is Ownable {
-    enum DocumentType { Asset, Provider }
+    enum ValueType { 
+        DID,                // DID string e.g. 'did:op:xxx'
+        DIDRef,             // hash of DID same as in parameter (bytes32 _did) in text 0x0123abc.. or 0123abc..
+        URL,                // URL string e.g. 'http(s)://xx'
+        DDO                 // DDO string in JSON e.g. '{ "id": "did:op:xxx"...
+    }
 
-    struct Identity {
+    struct DIDRegister {
         address owner;
-        DocumentType _type;
-        uint blockNumber;
+        uint updateAt;
     }
 
     event DIDAttributeRegistered(
         bytes32 indexed did,
         address indexed owner,
-        DocumentType _type,
+        ValueType _type,
         bytes32 indexed key,
         string value,
         uint updatedAt
     );
 
-    mapping(bytes32 => Identity) public identities;
+    mapping(bytes32 => Identity) public didRegister;
 
     constructor() Ownable() public {
     }
 
-    function registerAttribute(bytes32 _did, DocumentType _type, bytes32 _key, string _value) public {
+    function registerAttribute(bytes32 _did, ValueType _type, bytes32 _key, string _value) public {
         address currentOwner;
-        currentOwner = identities[_did].owner;
+        currentOwner = didRegister[_did].owner;
         require(currentOwner == address(0x0) || currentOwner == msg.sender, 'Attributes must be registered by the DID owners.');
 
-        identities[_did] = Identity(msg.sender, _type, block.number);
+        didRegister[_did] = DIDRegister(msg.sender, block.number);
         emit DIDAttributeRegistered(_did, msg.sender, _type, _key, _value, block.number);
     }
 }
