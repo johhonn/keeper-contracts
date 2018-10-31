26ef5e6f1145d7f05aa16abc32bb05faeafb6193
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-10-31 11:39:40 +0100

    Reverted testing changes

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/keeper-contracts',
-    version='0.3.3c',
+    version='0.3.3',
     zip_safe=False,
 )
 
