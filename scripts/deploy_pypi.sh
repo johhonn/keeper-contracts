76cb54fdf0ebc3de3ab815a6dd790bc1d3524b17
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-11-01 16:47:19 +0100

    Test pypi without tag

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -4,5 +4,4 @@ shopt -s nullglob
 abifiles=( ./artifacts/*.development.json )
 [ "${#abifiles[@]}" -lt "1" ] && echo "ABI Files for development environment not found" && exit 1
 python3 setup.py sdist bdist_wheel
-twine upload dist/*
-
+# twine upload dist/*
