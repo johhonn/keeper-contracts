59791b6e1ef6dbb2cc55a61d8dba145085b6a034
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-11-05 08:26:53 +0100

    Test java package release

--- scripts/maven.sh
+++ scripts/maven.sh
@@ -2,10 +2,10 @@
 
 ## Generating web3j stubs
 
+shopt -s nullglob # Avoid literal evaluation if not files
 for file in build/contracts/*.json
 do
-    web3j truffle generate --javaTypes $file -o src/main/java -p com.oceanprotocol.keeper.contracts 
+    web3j truffle generate --javaTypes $file -o src/main/java -p com.oceanprotocol.keeper.contracts
 done
 
 mvn clean install
-
