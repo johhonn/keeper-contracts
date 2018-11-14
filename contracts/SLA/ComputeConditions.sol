ae8ebec903ebc82ebdf459b3d5137cece2de3635
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2018-11-14 14:35:35 +0100

    add documentation for computeConditions.sol

--- contracts/SLA/ComputeConditions.sol
+++ contracts/SLA/ComputeConditions.sol
@@ -3,6 +3,11 @@ pragma solidity ^0.4.25;
 import 'openzeppelin-solidity/contracts/cryptography/ECDSA.sol';
 import './ServiceAgreement.sol';
 
+/// @title On-premise compute conditions
+/// @author Ocean Protocol Team
+/// @notice This contract is WIP, don't use if for production
+/// @dev All function calls are currently implement without side effects
+
 contract ComputeConditions {
 
     struct UploadProof {
@@ -29,7 +34,6 @@ contract ComputeConditions {
 
     modifier onlyComputePublisher(bytes32 serviceAgreementId) {
         require(msg.sender == serviceAgreementStorage.getAgreementPublisher(serviceAgreementId), 'Invalid publisher address');
-        require(!proofs[serviceAgreementId].valid, 'avoid replay attack!');
         _;
 
     }
@@ -45,7 +49,12 @@ contract ComputeConditions {
         serviceAgreementStorage = ServiceAgreement(serviceAgreementAddress);
     }
 
-
+    /// @notice submitSignature is called only by the data-scientist address.
+    /// @dev At first It checks if the proof state is created or not then checks that the hash
+    /// has been submitted by the publisher in order to call fulfillUpload. This preserves
+    /// the message integrity and proof that both parties agree on the same algorithm file/s
+    /// @param serviceAgreementId , the service level agreement Id
+    /// @param signature data scientist signature = signed_hash(uploaded_algorithm_file/s)
     function submitSignature(bytes32 serviceAgreementId, bytes signature) public onlyDataScientist(serviceAgreementId) returns(bool) {
         if(proofs[serviceAgreementId].created){
             if(proofs[serviceAgreementId].locked) { // avoid race conditions
@@ -64,6 +73,12 @@ contract ComputeConditions {
 
     }
 
+    /// @notice submitHash is called only by the on-premise address.
+    /// @dev At first It checks if the proof state is created or not then checks if the signature
+    /// has been submitted by the data scientist in order to call fulfillUpload. This preserves
+    /// the message integrity and proof that both parties agree on the same algorithm file/s
+    /// @param serviceAgreementId the service level agreement Id
+    /// @param hash = kekccak(uploaded_algorithm_file/s)
     function submitHash(bytes32 serviceAgreementId, bytes32 hash) public onlyComputePublisher(serviceAgreementId) returns(bool) {
         if(proofs[serviceAgreementId].created){
             if(proofs[serviceAgreementId].locked) { // avoid race conditions
@@ -84,6 +99,12 @@ contract ComputeConditions {
         return true;
     }
 
+    /// @notice fulfillUpload is called by anyone of the stakeholders [publisher or data scientist]
+    /// @dev check if there are unfulfilled dependency condition, if false, it verifies the signature
+    /// using the submitted hash (by publisher), the signature (by data scientist) then call
+    /// fulfillCondition in service level agreement storage contract
+    /// @param serviceAgreementId the service level agreement Id
+    /// @param state get be used fo input value hash for this condition indicating the state of verification
     function fulfillUpload(bytes32 serviceAgreementId, bool state) public onlyStakeholders(serviceAgreementId) returns(bool) {
         bytes32 condition = serviceAgreementStorage.getConditionByFingerprint(serviceAgreementId, address(this), this.fulfillUpload.selector);
         if (serviceAgreementStorage.hasUnfulfilledDependencies(serviceAgreementId, condition)){
