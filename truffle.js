fbaafc2b8badd2010bbd3fad89985b474aaa83c4
Sebastian Gerske <sebastian.gerske@gmail.com>
2018-11-14 08:14:10 +0100

    deploy to kovan network

--- truffle.js
+++ truffle.js
@@ -1,8 +1,5 @@
 const HDWalletProvider = require('truffle-hdwallet-provider')
 
-// etherbase of that nmemoric: 0x2c0d5f47374b130ee398f4c34dbe8168824a8616
-const nmemoric = process.env.KOVAN_NMEMORIC
-
 module.exports = {
     networks: {
         development: {
@@ -33,7 +30,7 @@ module.exports = {
             from: '0x64137aF0104d2c96C44bb04AC06f09eC84CC5Ae4'
         },
         kovan: {
-            provider: () => new HDWalletProvider(nmemoric, `https://kovan.infura.io/v2/${process.env.INFURA_TOKEN}`),
+            provider: () => new HDWalletProvider(process.env.KOVAN_NMEMORIC, `https://kovan.infura.io/v2/${process.env.INFURA_TOKEN}`),
             network_id: '42'
         },
         coverage: {
