45a275e5562cf6f0e621c6dd3fed7e8a3f492656
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-01-07 13:46:43 +0100

    fix merge conflicts

--- contracts/OceanToken.sol
+++ contracts/OceanToken.sol
@@ -1,6 +1,7 @@
 pragma solidity 0.4.25;
 
-import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Capped.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol';
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 
 /**
