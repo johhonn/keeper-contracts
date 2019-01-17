dad1527742749c04c04f4831a72e2d763c178cc2
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-01-17 11:57:39 +0100

    adapt token test to changes

--- contracts/testContracts/OceanToken/OceanTokenChangeInStorageAndLogic.sol
+++ contracts/testContracts/OceanToken/OceanTokenChangeInStorageAndLogic.sol
@@ -1,74 +1,76 @@
-/* solium-disable */
 pragma solidity 0.4.25;
 
-import 'openzeppelin-eth/contracts/token/ERC20/ERC20.sol';
+import 'openzeppelin-eth/contracts/token/ERC20/ERC20Capped.sol';
+import 'openzeppelin-eth/contracts/token/ERC20/ERC20Detailed.sol';
+import 'zos-lib/contracts/Initializable.sol';
 
-contract OceanTokenChangeInStorageAndLogic is ERC20 {
+
+/**
+ * @title Ocean Protocol ERC20 Token Contract
+ * @author Ocean Protocol Team
+ * @dev All function calls are currently implemented without side effects
+ */
+contract OceanTokenChangeInStorageAndLogic
+is Initializable, ERC20Detailed, ERC20Capped
+{
 
     using SafeMath for uint256;
 
-    // ============
-    // DATA STRUCTURES:
-    // ============
-    string public name;             // Set the token name for display
-    string public symbol;           // Set the token symbol for display
-
-    // SUPPLY
-    uint8 public decimals;          // Set the number of decimals for display
-    uint256 public TOTAL_SUPPLY;    // OceanToken total supply
-
-    // EMIT TOKENS
-    address public _receiver;       // address to receive TOKENS
-    uint256 public totalSupply;     // total supply of Ocean tokens including initial tokens plus block rewards
     // keep track of how many times a function was called.
     mapping (address=>uint256) public called;
-    /**
-    * @dev OceanToken initializer
-    * Runs only once after initial deploy.
-    */
-    function initialize() public initializer() {
-        totalSupply = TOTAL_SUPPLY;
-        name = 'OceanToken';                     // Set the token name for display
-        symbol = 'OCN';                          // Set the token symbol for display
-        decimals = 18;                           // Set the number of decimals for display
-        TOTAL_SUPPLY = 1400000000 * 10 ** 18;    // OceanToken total supply
-        _receiver = address(0);                  // address to receive TOKENS
-    }
 
     /**
-    * @dev setReceiver set the address to receive the emitted tokens
-    * @param _to The address to send tokens
-    * @return success setting is successful.
+    * @dev OceanToken Initializer
+    * Runs only on initial contract creation.
     */
-    function setReceiver(address _to) public returns (bool success){
-        // make sure receiver is not set already
-        require(_receiver == address(0), 'Receiver address already set.');
-        // Creator address is assigned initial available tokens
-        super._mint(_to, TOTAL_SUPPLY);
-        // set receiver
-        _receiver = _to;
-        return true;
+    function initialize(
+        address _minter
+    )
+    public initializer()
+    {
+        ERC20Detailed.initialize('OceanToken', 'OCN', 18);
+        ERC20Capped.initialize(1400000000 * 10 ** 18, _minter);
     }
 
     /**
     * @dev Transfer token for a specified address when not paused
-    * @param _to The address to transfer to.
-    * @param _value The amount to be transferred.
+    * @param to The address to transfer to.
+    * @param value The amount to be transferred.
     */
-    function transfer(address _to, uint256 _value) public returns (bool) {
+    function transfer(
+        address to,
+        uint256 value
+    )
+    public
+    returns (bool tokenTransferred)
+    {
         called[msg.sender] += 1;
-        require(_to != address(0), 'To address is 0x0.');
-        return super.transfer(_to, _value);
+        require(
+            to != address(0),
+            'To address is 0x0.'
+        );
+        return super.transfer(to, value);
     }
 
     /**
     * @dev Transfer tokens from one address to another when not paused
-    * @param _from address The address which you want to send tokens from
-    * @param _to address The address which you want to transfer to
-    * @param _value uint256 the amount of tokens to be transferred
+    * @param from address The address which you want to send tokens from
+    * @param to address The address which you want to transfer to
+    * @param value uint256 the amount of tokens to be transferred
     */
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-        require(_to != address(0), 'To address is 0x0.');
-        return super.transferFrom(_from, _to, _value);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    )
+    public
+    returns (bool tokenTransferred)
+    {
+        require(
+            to != address(0),
+            'To address is 0x0.'
+        );
+        return super.transferFrom(from, to, value);
     }
+
 }
