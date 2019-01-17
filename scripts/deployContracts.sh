395d165dcec187ef598d6e9037d8705af4068122
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-01-17 09:42:27 +0100

    added more logging, added bailing

--- scripts/deployContracts.sh
+++ scripts/deployContracts.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+set -ex
+
 # -----------------------------------------------------------------------
 # Script configuration
 # -----------------------------------------------------------------------
@@ -30,26 +32,27 @@ rm -f zos.*
 # Project setup using zOS
 # -----------------------------------------------------------------------
 # List of contracts
-declare -a contracts=("DIDRegistry" "OceanToken" "Dispenser" "ServiceExecutionAgreement" "AccessConditions" "PaymentConditions" "FitchainConditions" "ComputeConditions")
+declare -a contracts=("DIDRegistry" "OceanToken" "Dispenser" "ServiceExecutionAgreement" "AccessConditions" "FitchainConditions" "ComputeConditions")
 # Initialize project zOS project
 # NOTE: Creates a zos.json file that keeps track of the project's details
 npx zos init oceanprotocol $VERSION -v
 # Register contracts in the project as an upgradeable contract.
 for contract in "${contracts[@]}"
 do
-    npx zos add $contract -v --skip-compile
+    npx zos add $contract --skip-compile -v
 done
 
 # Deploy all implementations in the specified network.
 # NOTE: Creates another zos.<network_name>.json file, specific to the network used,
 # which keeps track of deployed addresses, etc.
-npx zos push --skip-compile  -v
+npx zos push --skip-compile -v
+
 # Request a proxy for the upgradeably contracts.
 # Here we run initialize which replace contract constructors
 # Since each contract initialize function could be different we can not use a loop
 # NOTE: A dapp could now use the address of the proxy specified in zos.<network_name>.json
 # instance=MyContract.at(proxyAddress)
-npx zos create DIDRegistry --init initialize -v
+npx zos create DIDRegistry --init initialize --args $ADMIN -v
 tokenAddress=$(npx zos create OceanToken --init --args $ADMIN -v)
 npx zos create Dispenser --init initialize --args $tokenAddress,$ADMIN -v
 serviceExecutionAgreementAddress=$(npx zos create ServiceExecutionAgreement -v)
