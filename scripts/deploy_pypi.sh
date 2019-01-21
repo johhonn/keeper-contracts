1aa3c0bce1d02d58beba200522b7a3437017dae9
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-01-21 16:29:36 +0100

    Fix PyPi package

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -4,6 +4,7 @@ pyenv global system 3.6.7
 pip3 install --upgrade pip
 pip3 install --upgrade twine six==1.10.0 wheel==0.31.0 setuptools
 pip3 list
+sudo apt-get update && sudo apt-get install -y python3-pip
 shopt -s nullglob
 abifiles=( ./artifacts/*.development.json )
 [ "${#abifiles[@]}" -lt "1" ] && echo "ABI Files for development environment not found" && exit 1
