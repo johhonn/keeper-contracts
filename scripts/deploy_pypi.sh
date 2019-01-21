a256c55773fb679da7beead4a4e0fc3ee8e8f519
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-01-21 19:12:25 +0100

    Moving setup.py to python2

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-pip3 install --upgrade --user travis pip
-pip3 install --upgrade --user travis twine six==1.10.0 wheel==0.31.0 setuptools
-pip3 list
+pip install --upgrade --user travis pip
+pip install --upgrade --user travis twine six==1.10.0 wheel==0.31.0 setuptools
+pip list
 shopt -s nullglob
 abifiles=( ./artifacts/*.development.json )
 [ "${#abifiles[@]}" -lt "1" ] && echo "ABI Files for development environment not found" && exit 1
-python3 setup.py sdist bdist_wheel
+python setup.py sdist bdist_wheel
 twine upload dist/*
