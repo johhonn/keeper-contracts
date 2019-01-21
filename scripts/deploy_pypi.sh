1c317f0fe594a142dab4d578d6816b756299dba1
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-01-21 10:25:01 +0100

    Fix setuptools pip

--- scripts/deploy_pypi.sh
+++ scripts/deploy_pypi.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pyenv global system 3.6.7
+pip3 install setuptools
 shopt -s nullglob
 abifiles=( ./artifacts/*.development.json )
 [ "${#abifiles[@]}" -lt "1" ] && echo "ABI Files for development environment not found" && exit 1
