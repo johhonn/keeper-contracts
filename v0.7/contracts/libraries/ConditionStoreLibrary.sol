be99cecb5cd04b98549b28c442896076c0b91c31
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2019-01-31 13:02:58 +0200

    link libraries

--- v0.7/contracts/libraries/ConditionStoreLibrary.sol
+++ v0.7/contracts/libraries/ConditionStoreLibrary.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.5.0;
 
-import './EpochLibrary.sol';
+import { EpochLibrary } from './EpochLibrary.sol';
 
 library ConditionStoreLibrary {
 
--- v0.7/migrations/3_deploy_condition_store_library.js
+++ v0.7/migrations/3_deploy_condition_store_library.js
@@ -1,8 +1,13 @@
 /* global artifacts */
+const EpochLibrary = artifacts.require('EpochLibrary.sol')
 const ConditionStoreLibrary = artifacts.require('ConditionStoreLibrary.sol')
 const { saveDefinition } = require('./helper')
 
 const conditionStoreLibrary = async (deployer, network) => {
+    await deployer.deploy(
+        EpochLibrary
+    )
+    deployer.link(EpochLibrary, ConditionStoreLibrary);
     await deployer.deploy(
         ConditionStoreLibrary
     )
