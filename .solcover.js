4e52f9db1e002c1d2324a8a3a161a8b918a970e3
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-01-31 16:23:48 +0100

    exclude ugradability tests from solcover

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
     compileCommand: 'npm run compile -- --all',
-    testCommand: 'export ETHEREUM_RPC_PORT=8555&& npm run test -- --network coverage --timeout 10000',
+    testCommand: 'export ETHEREUM_RPC_PORT=8555&& npm run test:unit -- --network coverage --timeout 10000&& npm run test:integration -- --network coverage --timeout 10000',
     copyPackages: ['openzeppelin-eth'],
     skipFiles: ['testContracts'],
 }
