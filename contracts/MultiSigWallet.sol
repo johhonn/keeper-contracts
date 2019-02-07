f18121ac7cbaa06f94566cb5e37bcac2deffa527
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-02-07 13:30:26 +0100

    separate wallet from keeper contracts

--- contracts/MultiSigWallet.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.5.3;
-
-import 'gnosis-safe/contracts/GnosisSafe.sol';
-
-/// @title Multisignature wallet - Allows multiple parties to agree on transactions before execution.
-/// @author Stefan George - <stefan.george@consensys.net>
-contract MultiSigWallet is GnosisSafe { }
