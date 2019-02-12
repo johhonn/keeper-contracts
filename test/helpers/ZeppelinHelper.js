f7387b97b8b55116611c53aa153db49e6a3d55d2
diminator <dimi.dejonghe@gmail.com>
2019-02-13 00:04:26 +0100

    (add) didregistry upgrade test

--- test/helpers/ZeppelinHelper.js
+++ test/helpers/ZeppelinHelper.js
@@ -6,45 +6,40 @@ const { encodeCall } = require('zos-lib')
 const { execSync } = require('child_process')
 const glob = require('glob')
 const fs = require('fs')
-const contract = require('truffle-contract')
+const loadWallet = require('./loadWallet')
+const createWallet = require('./createWallet')
 
 const verbose = process.env.VERBOSE || false
-const MultiSigWallet = contract(require('@oceanprotocol/multisigwallet/build/contracts/MultiSigWalletWithDailyLimit'))
 const flags = verbose ? ' -v' : ' -s'
 const stdio = 'inherit'
+const network = process.env.NETWORK
 
 class ZeppelinHelperBase {
     async restoreState(admin) {
         if (!admin) {
             throw new Error('no account to restore state')
         }
-        // remove config files
-        try {
-            execSync('rm -f zos.* .zos.*', { stdio: 'ignore' })
-            if (verbose) {
-                console.log('Cleaned zos session.')
-            }
-        } catch (e) {
-            console.log(e)
-            console.log('Continuing anyways')
-        }
-        // set zos session parameters
-        // we need to identify if the network is coverage or if its development
-        let network = process.env.NETWORK
-        if (verbose) {
-            console.log(`Creating session with network: ${network} and admin: ${admin}`)
-        }
-        execSync(`npx zos session --network ${network} --from ${admin} ${flags}`)
 
+        execSync('rm -f ./zos.* ./.zos.*', { stdio: 'ignore' })
+        console.log('Cleaned zos session.')
+        await createWallet()
+        // Get wallet address
+        const adminWallet = await loadWallet('upgrader') // zos admin MultiSig
+        const ownerWallet = await loadWallet('owner') // contract admin
+
+        // Set zos session (network, admin, timeout)
+        // execSync(`npx zos session --network ${network} --from ${admin} --expires ${timeout}`)
+        console.log(`Creating session with network: ${network} and admin: ${admin}`)
+        execSync(`npx zos session --network ${network} --from ${admin} ${flags}`)
         execSync(`npx zos init oceanprotocol v${pkg.version} --force ${flags}`)
         // add all contracts again
-        for (let contract of ['DIDRegistry', 'OceanToken', 'Dispenser',
-            'ServiceExecutionAgreement', 'AccessConditions', 'PaymentConditions',
-            'FitchainConditions', 'ComputeConditions']) {
-            execSync(`npx zos add ${contract} --skip-compile ${flags}`)
-        }
+        console.log('adding contracts')
+        const contracts = ['DIDRegistry', 'OceanToken', 'Dispenser']
+        execSync(`npx zos add ${contracts.join(' ')} --skip-compile -v`)
         // push contracts
+        console.log('pushing contracts')
         execSync(`npx zos push --skip-compile ${flags}`)
+        return adminWallet
     }
 }
 
@@ -55,12 +50,7 @@ module.exports = class ZeppelinHelper extends ZeppelinHelperBase {
         this.dependencies = {
             DIDRegistry: [],
             OceanToken: [],
-            Dispenser: ['OceanToken'],
-            ServiceExecutionAgreement: [],
-            AccessConditions: ['ServiceExecutionAgreement'],
-            PaymentConditions: ['ServiceExecutionAgreement', 'OceanToken'],
-            FitchainConditions: ['ServiceExecutionAgreement'],
-            ComputeConditions: ['ServiceExecutionAgreement']
+            Dispenser: ['OceanToken']
         }
     }
 
@@ -74,7 +64,7 @@ module.exports = class ZeppelinHelper extends ZeppelinHelperBase {
         this.addresses = {}
         // Wallet will be deployed only if upgrade test will be perform
         if (upgrade) {
-            await this.loadWallet()
+            this.wallet = await loadWallet('upgrader')
         }
         await this.createContract(this.contractName)
     }
@@ -102,21 +92,6 @@ module.exports = class ZeppelinHelper extends ZeppelinHelperBase {
                     break
                 case 'Dispenser':
                     cmd = `Dispenser --init initialize --args ${this.addresses['OceanToken']},${this.owner}`
-                    break
-                case 'ServiceExecutionAgreement':
-                    cmd = `ServiceExecutionAgreement`
-                    break
-                case 'PaymentConditions':
-                    cmd = `PaymentConditions --init initialize --args ${this.addresses['ServiceExecutionAgreement']},${this.addresses['OceanToken']}`
-                    break
-                case 'AccessConditions':
-                    cmd = `AccessConditions --init initialize --args ${this.addresses['ServiceExecutionAgreement']}`
-                    break
-                case 'ComputeConditions':
-                    cmd = `ComputeConditions --init initialize --args ${this.addresses['ServiceExecutionAgreement']}`
-                    break
-                case 'FitchainConditions':
-                    cmd = `FitchainConditions --init initialize --args ${this.addresses['ServiceExecutionAgreement']},10,1`
                     break
                 default:
                     throw Error(`${contract} Not implemented in create`)
@@ -148,38 +123,25 @@ module.exports = class ZeppelinHelper extends ZeppelinHelperBase {
         }
     }
 
-    async loadWallet() {
-        let files = glob.sync('./wallets.json')
-        if (files === undefined || files.length === 0) {
-            // array empty or does not exist
-            throw Error(`wallet config file not found`)
-        }
-        /* eslint-disable-next-line security/detect-non-literal-fs-filename */
-        const wallet = JSON.parse(fs.readFileSync(files[0]))
-        const walletAddress = wallet.wallet
-        this.wallet = await MultiSigWallet.at(walletAddress)
-        this.walletOwners = wallet.owners
-    }
-
     getProxyAddress(contractName) {
         return this.addresses[contractName]
     }
 
-    async upgradeToNewContract(newContractName) {
+    async upgradeToNewContract(newContractName, account) {
         execSync(`npx zos add ${newContractName}:${this.contractName} --skip-compile ${flags}`, { stdio: stdio })
         execSync(`npx zos push --skip-compile --force ${flags}`, { stdio: stdio })
         await this.readImplementationFromJson()
         const upgradeCallData = encodeCall('upgradeTo', ['address'], [this.implementationAddress])
-        let tx = await this.wallet
-            .submitTransaction(
-                this.addresses[this.contractName],
-                0,
-                upgradeCallData,
-                { from: this.walletOwners[0] })
+        let tx = await this.wallet.submitTransaction(
+            this.addresses[this.contractName], 0, upgradeCallData,
+            { from: account }
+        )
         this.latestTransaction = tx.logs[0].args.transactionId.toNumber()
     }
 
-    async approveLatestTransaction() {
-        await this.wallet.confirmTransaction(this.latestTransaction, { from: this.walletOwners[1] })
+    async approveLatestTransaction(account) {
+        await this.wallet.confirmTransaction(
+            this.latestTransaction, { from: account }
+        )
     }
 }
