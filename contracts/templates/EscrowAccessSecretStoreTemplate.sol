26563b7c2f9454222b240a3e29d25269bd57aa85
diminator <dimi.dejonghe@gmail.com>
2019-02-14 17:13:12 +0100

    Template base class

--- contracts/templates/EscrowAccessSecretStoreTemplate.sol
+++ contracts/templates/EscrowAccessSecretStoreTemplate.sol
@@ -1,21 +1,13 @@
 pragma solidity 0.5.3;
 
-import './TemplateStoreLibrary.sol';
-import '../agreements/AgreementStoreManager.sol';
-import '../agreements/AgreementStoreManager.sol';
 import '../conditions/AccessSecretStoreCondition.sol';
 import '../conditions/LockRewardCondition.sol';
 import '../conditions/rewards/EscrowReward.sol';
-import 'openzeppelin-eth/contracts/ownership/Ownable.sol';
+import './Template.sol';
 
-contract EscrowAccessSecretStoreTemplate is Ownable {
 
-    using TemplateStoreLibrary for TemplateStoreLibrary.TemplateList;
+contract EscrowAccessSecretStoreTemplate is Template {
 
-    TemplateStoreLibrary.TemplateList private templateList;
-    address[] private conditionTypes;
-
-    AgreementStoreManager private agreementStoreManager;
     AccessSecretStoreCondition private accessSecretStoreCondition;
     LockRewardCondition private lockRewardCondition;
     EscrowReward private escrowReward;
@@ -56,35 +48,5 @@ contract EscrowAccessSecretStoreTemplate is Ownable {
         conditionTypes.push(address(accessSecretStoreCondition));
         conditionTypes.push(address(lockRewardCondition));
         conditionTypes.push(address(escrowReward));
-    }
-
-    function createAgreement(
-        bytes32 _id,
-        bytes32 _did,
-        address _didOwner,
-        bytes32[] memory _conditionIds,
-        uint[] memory _timeLocks,
-        uint[] memory _timeOuts
-    )
-        public
-        returns (uint size)
-    {
-        return agreementStoreManager.createAgreement(
-            _id,
-            _did,
-            _didOwner,
-            getConditionTypes(),
-            _conditionIds,
-            _timeLocks,
-            _timeOuts
-        );
-    }
-
-    function getConditionTypes()
-        public
-        view
-        returns (address[] memory)
-    {
-        return conditionTypes;
     }
 }
