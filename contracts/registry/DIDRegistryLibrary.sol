037f3e63e95a0465fd88d6ced96dcd7adc86c625
diminator <dimi.dejonghe@gmail.com>
2019-02-18 16:37:23 +0100

    (doc) DIDRegistry docs

--- contracts/registry/DIDRegistryLibrary.sol
+++ contracts/registry/DIDRegistryLibrary.sol
@@ -1,8 +1,9 @@
 pragma solidity 0.5.3;
 
 /**
- * @title DID Registry
+ * @title DID Registry Library
  * @author Ocean Protocol Team
+ *
  * @dev All function calls are currently implemented without side effects
  */
 library DIDRegistryLibrary {
@@ -19,9 +20,10 @@ library DIDRegistryLibrary {
         bytes32[] didRegisterIds;
     }
 
-
    /**
-    * @dev this function updates DID attributes
+    * @notice update the DID store
+    * @dev access modifiers and storage pointer should be implemented in DIDRegistry
+    * @param _self refers to storage pointer
     * @param _did refers to decentralized identifier (a byte32 length ID)
     * @param _checksum includes a one-way HASH calculated using the DDO content
     */
