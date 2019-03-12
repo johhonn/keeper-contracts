6074bcbda97c52e9b2d2c6fc4d2b5b43c5ff31d5
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-03-12 15:09:57 +0100

    use nonce tracker only in cases we need it (if you connect to a server farm)

--- truffle.js
+++ truffle.js
@@ -4,19 +4,31 @@ const NonceTrackerSubprovider = require('web3-provider-engine/subproviders/nonce
 const rpcHost = process.env.KEEPER_RPC_HOST
 const rpcPort = process.env.KEEPER_RPC_PORT
 const url = process.env.KEEPER_RPC_URL
+
 const walletIndex = 0
 const walletAccounts = 5
 
 let wallet
 
-const setupWallet = (nmemoric, url) => {
+const setupWallet = (
+    nmemoric,
+    url,
+    useNonceTracker
+) => {
     if (!wallet) {
         wallet = new HDWalletProvider(
             nmemoric,
             url,
             walletIndex,
             walletAccounts)
-        wallet.engine.addProvider(new NonceTrackerSubprovider())
+        if (useNonceTracker) {
+            /*
+             * This is only required if you connect to infura or nile nodes
+             * For details see:
+             * https://ethereum.stackexchange.com/questions/44349/truffle-infura-on-mainnet-nonce-too-low-error/50038#50038
+             */
+            wallet.engine.addProvider(new NonceTrackerSubprovider())
+        }
     }
     return wallet
 }
@@ -44,16 +56,21 @@ module.exports = {
         spree: {
             provider: () => setupWallet(
                 process.env.NMEMORIC,
-                url || `http://localhost:8545`),
+                url || `http://localhost:8545`,
+                false
+            ),
             network_id: 0x2324,
-            gas: 4500000,
+            gas: 60000000,
+            gasPrice: 10000,
             from: '0xe2DD09d719Da89e5a3D0F2549c7E24566e947260'
         },
         // nile the ocean testnet
         nile: {
             provider: () => setupWallet(
                 process.env.NMEMORIC,
-                url || `https://nile.dev-ocean.com`),
+                url || `https://nile.dev-ocean.com`,
+                true
+            ),
             network_id: 0x2323,
             gas: 6000000,
             gasPrice: 10000,
@@ -63,7 +80,9 @@ module.exports = {
         kovan: {
             provider: () => setupWallet(
                 process.env.NMEMORIC,
-                url || `https://kovan.infura.io/v2/${process.env.INFURA_TOKEN}`),
+                url || `https://kovan.infura.io/v2/${process.env.INFURA_TOKEN}`,
+                true
+            ),
             network_id: '42',
             from: '0x2c0D5F47374b130EE398F4C34DBE8168824A8616'
         }
