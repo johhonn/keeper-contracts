93fc321a710a2f84ef4446696e6ff11b5ea770fa
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-03-13 12:06:05 +0100

    v0.8.5

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/oceanprotocol/keeper-contracts',
-    version='0.8.4',
+    version='0.8.5',
     zip_safe=False,
 )
 
