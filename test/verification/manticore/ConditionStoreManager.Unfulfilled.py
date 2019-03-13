5fc2338d6fe855aa2a417b0ba49831d2bc7aa1b1
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2019-03-13 12:20:00 +0100

    fix codacy

--- test/verification/manticore/ConditionStoreManager.Unfulfilled.py
+++ test/verification/manticore/ConditionStoreManager.Unfulfilled.py
@@ -24,18 +24,18 @@ if __name__ == '__main__':
     m = ManticoreEVM()
 
     owner_account = m.create_account(balance=1000, name='owner_account')
-    print(f'[+] Created owner account "{owner_account.name_}"')
+    print(f'[+] Created owner account ', owner_account.name_)
 
     creator_account = m.create_account(balance=1000, name='creator_owner_account')
-    print(f'[+] Created creator account "{creator_account.name_}"')
+    print(f'[+] Created creator account ',creator_account.name_)
 
     _, contract_account = create_condition_store_manager(m, owner_account, EPOCH_LIBRARY_JSON_PATH, CONDITION_STORE_MANAGER_JSON_PATH)
 
     contract_account.initialize(owner_account, creator_account, caller=owner_account, value=0, signature='(address,address)')
-    print(f'[+] Initialized contract {CONDITION_STORE_MANAGER_JSON_PATH[len(ROOT_DIR):]}')
+    print(f'[+] Initialized contract ', CONDITION_STORE_MANAGER_JSON_PATH[len(ROOT_DIR):])
 
     condition_owner_account = m.create_account(balance=1000, name='condition_owner_account')
-    print(f'[+] Created condition owner account "{condition_owner_account.name_}"')
+    print(f'[+] Created condition owner account ',condition_owner_account.name_)
 
     symbolic_value_1 = m.make_symbolic_value()
     symbolic_value_2 = m.make_symbolic_value()
@@ -43,7 +43,8 @@ if __name__ == '__main__':
     contract_account.createCondition("condition1", condition_owner_account, symbolic_value_1, symbolic_value_2, signature='(bytes32,address,uint256,uint256)', caller=creator_account, value=0)
 
     running_states = list(m.running_states) 
-    assert(len(running_states) > 0)
+    if not (len(running_states) > 0):
+        raise AssertionError()
 
     contract_account.getConditionState("condition1", caller=creator_account, value=0)
     UNFULFILLED = 1
@@ -53,17 +54,19 @@ if __name__ == '__main__':
         ret = tx.return_data
         ret = Operators.CONCAT(256, *ret)
         #print(ret)
-        assert(not m.generate_testcase(running_states[0], '', only_if=(ret != UNFULFILLED)))
+        if m.generate_testcase(running_states[0], '', only_if=(ret != UNFULFILLED)):
+            raise AssertionError()
 
     attacker_account = m.create_account(balance=1000, name='attacker_account')
-    print(f'[+] Created attacker account "{attacker_account.name_}"')
+    print(f'[+] Created attacker account ',attacker_account.name_)
 
     symbolic_value_1 = m.make_symbolic_value()
     contract_account.updateConditionState("condition1", symbolic_value_1, caller=attacker_account)
 
     running_states = list(m.running_states) 
-    assert(len(running_states) == 0)
+    if not (len(running_states) == 0):
+        raise AssertionError()
 
     m.finalize()
-    print(f"[+] Look for results in {m.workspace}")
+    print(f"[+] Look for results in ", m.workspace)
 
