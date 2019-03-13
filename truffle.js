a92ad4694c10b2b64d5d85f827ffcb3aa77655da
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-03-13 12:04:38 +0100

    rollback nonce tracker change, it can be used always

--- truffle.js
+++ truffle.js
@@ -12,8 +12,7 @@ let hdWalletProvider
 
 const setupWallet = (
     nmemoric,
-    url,
-    useNonceTracker
+    url
 ) => {
     if (!hdWalletProvider) {
         hdWalletProvider = new HDWalletProvider(
@@ -21,14 +20,7 @@ const setupWallet = (
             url,
             hdWalletStartIndex,
             hdWalletAccounts)
-        if (useNonceTracker) {
-            /*
-             * This is only required if you connect to infura or nile nodes
-             * For details see:
-             * https://ethereum.stackexchange.com/questions/44349/truffle-infura-on-mainnet-nonce-too-low-error/50038#50038
-             */
-            hdWalletProvider.engine.addProvider(new NonceTrackerSubprovider())
-        }
+        hdWalletProvider.engine.addProvider(new NonceTrackerSubprovider())
     }
     return hdWalletProvider
 }
@@ -56,8 +48,7 @@ module.exports = {
         spree: {
             provider: () => setupWallet(
                 process.env.NMEMORIC,
-                url || `http://localhost:8545`,
-                false
+                url || `http://localhost:8545`
             ),
             network_id: 0x2324, // 8996
             gas: 60000000,
@@ -68,8 +59,7 @@ module.exports = {
         nile: {
             provider: () => setupWallet(
                 process.env.NMEMORIC,
-                url || `https://nile.dev-ocean.com`,
-                true
+                url || `https://nile.dev-ocean.com`
             ),
             network_id: 0x2323, // 8995
             gas: 6000000,
@@ -80,8 +70,7 @@ module.exports = {
         kovan: {
             provider: () => setupWallet(
                 process.env.NMEMORIC,
-                url || `https://kovan.infura.io/v2/${process.env.INFURA_TOKEN}`,
-                true
+                url || `https://kovan.infura.io/v2/${process.env.INFURA_TOKEN}`
             ),
             network_id: 0x2A, // 42
             from: '0x2c0D5F47374b130EE398F4C34DBE8168824A8616'
