aa8ba7c9e7b62a00af14b442d608b9763a866f56
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-03-15 16:06:44 +0100

    solidity 0.5.6

--- truffle.js
+++ truffle.js
@@ -78,7 +78,7 @@ module.exports = {
     },
     compilers: {
         solc: {
-            version: '0.5.3',
+            version: '0.5.6',
             settings: {
                 optimizer: {
                     enabled: true,
