0d686a698d7b4d650484e78259d88a788f306c0b
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-05-07 11:53:58 +0200

    added a way to selectively deploy contracts

--- scripts/deploy/contracts/initializeContracts.js
+++ scripts/deploy/contracts/initializeContracts.js
@@ -17,6 +17,16 @@ async function initializeContracts(
 
     const addressBook = {}
 
+    // WARNING!
+    // use this only when deploying a selective portion of the contracts
+    // Only use this if you know what you do, otherwise it can break the contracts deployed
+    const proxies = {}
+
+    // returns either the address from the address book or the address of the manual set proxies
+    const getAddress = (contract) => {
+        return addressBook[contract] || proxies[contract]
+    }
+
     if (contracts.indexOf('DIDRegistry') > -1) {
         addressBook['DIDRegistry'] = zosCreate(
             'DIDRegistry',
@@ -36,12 +46,12 @@ async function initializeContracts(
         )
     }
 
-    if (addressBook['OceanToken']) {
+    if (getAddress('OceanToken')) {
         if (contracts.indexOf('Dispenser') > -1) {
             addressBook['Dispenser'] = zosCreate(
                 'Dispenser',
                 [
-                    addressBook['OceanToken'],
+                    getAddress('OceanToken'),
                     roles.ownerWallet
                 ],
                 verbose
@@ -65,102 +75,127 @@ async function initializeContracts(
         )
     }
 
-    if (addressBook['ConditionStoreManager']) {
+    if (getAddress('ConditionStoreManager')) {
         if (contracts.indexOf('SignCondition') > -1) {
             addressBook['SignCondition'] = zosCreate(
                 'SignCondition',
                 [
                     roles.ownerWallet,
-                    addressBook['ConditionStoreManager']
+                    getAddress('ConditionStoreManager')
                 ],
                 verbose
             )
         }
+
         if (contracts.indexOf('HashLockCondition') > -1) {
             addressBook['HashLockCondition'] = zosCreate(
                 'HashLockCondition',
                 [
                     roles.ownerWallet,
-                    addressBook['ConditionStoreManager']
+                    getAddress('ConditionStoreManager')
+                ],
+                verbose
+            )
+        }
+
+        if (contracts.indexOf('WhitelistingCondition') > -1) {
+            addressBook['WhitelistingCondition'] = zosCreate(
+                'WhitelistingCondition',
+                [
+                    roles.ownerWallet,
+                    getAddress('ConditionStoreManager')
                 ],
                 verbose
             )
         }
     }
 
-    if (addressBook['ConditionStoreManager'] &&
-        addressBook['TemplateStoreManager'] &&
-        addressBook['DIDRegistry']) {
+    if (getAddress('ConditionStoreManager') &&
+        getAddress('TemplateStoreManager') &&
+        getAddress('DIDRegistry')) {
         if (contracts.indexOf('AgreementStoreManager') > -1) {
             addressBook['AgreementStoreManager'] = zosCreate(
                 'AgreementStoreManager',
                 [
                     roles.ownerWallet,
-                    addressBook['ConditionStoreManager'],
-                    addressBook['TemplateStoreManager'],
-                    addressBook['DIDRegistry']
+                    getAddress('ConditionStoreManager'),
+                    getAddress('TemplateStoreManager'),
+                    getAddress('DIDRegistry')
                 ],
                 verbose
             )
         }
     }
 
-    if (addressBook['ConditionStoreManager'] &&
-        addressBook['OceanToken']) {
+    if (getAddress('ConditionStoreManager') &&
+        getAddress('OceanToken')) {
         if (contracts.indexOf('LockRewardCondition') > -1) {
             addressBook['LockRewardCondition'] = zosCreate(
                 'LockRewardCondition',
                 [
                     roles.ownerWallet,
-                    addressBook['ConditionStoreManager'],
-                    addressBook['OceanToken']
+                    getAddress('ConditionStoreManager'),
+                    getAddress('OceanToken')
                 ],
                 verbose
             )
         }
+
         if (contracts.indexOf('EscrowReward') > -1) {
             addressBook['EscrowReward'] = zosCreate(
                 'EscrowReward',
                 [
                     roles.ownerWallet,
-                    addressBook['ConditionStoreManager'],
-                    addressBook['OceanToken']
+                    getAddress('ConditionStoreManager'),
+                    getAddress('OceanToken')
                 ],
                 verbose
             )
         }
     }
 
-    if (addressBook['ConditionStoreManager'] &&
-        addressBook['AgreementStoreManager']) {
+    if (getAddress('ConditionStoreManager') &&
+        getAddress('AgreementStoreManager')) {
         if (contracts.indexOf('AccessSecretStoreCondition') > -1) {
             addressBook['AccessSecretStoreCondition'] = zosCreate(
                 'AccessSecretStoreCondition',
                 [
                     roles.ownerWallet,
-                    addressBook['ConditionStoreManager'],
-                    addressBook['AgreementStoreManager']
+                    getAddress('ConditionStoreManager'),
+                    getAddress('AgreementStoreManager')
+                ],
+                verbose
+            )
+        }
+
+        if (contracts.indexOf('EthereumAddressWhitelistingCondition') > -1) {
+            addressBook['EthereumAddressWhitelistingCondition'] = zosCreate(
+                'EthereumAddressWhitelistingCondition',
+                [
+                    roles.ownerWallet,
+                    getAddress('ConditionStoreManager'),
+                    getAddress('AgreementStoreManager')
                 ],
                 verbose
             )
         }
     }
 
-    if (addressBook['AgreementStoreManager'] &&
-        addressBook['DIDRegistry'] &&
-        addressBook['AccessSecretStoreCondition'] &&
-        addressBook['LockRewardCondition'] &&
-        addressBook['EscrowReward']) {
+    if (getAddress('AgreementStoreManager') &&
+        getAddress('DIDRegistry') &&
+        getAddress('AccessSecretStoreCondition') &&
+        getAddress('LockRewardCondition') &&
+        getAddress('EscrowReward')) {
         if (contracts.indexOf('EscrowAccessSecretStoreTemplate') > -1) {
             addressBook['EscrowAccessSecretStoreTemplate'] = zosCreate(
                 'EscrowAccessSecretStoreTemplate',
                 [
                     roles.ownerWallet,
-                    addressBook['AgreementStoreManager'],
-                    addressBook['DIDRegistry'],
-                    addressBook['AccessSecretStoreCondition'],
-                    addressBook['LockRewardCondition'],
-                    addressBook['EscrowReward']
+                    getAddress('AgreementStoreManager'),
+                    getAddress('DIDRegistry'),
+                    getAddress('AccessSecretStoreCondition'),
+                    getAddress('LockRewardCondition'),
+                    getAddress('EscrowReward')
                 ],
                 verbose
             )
