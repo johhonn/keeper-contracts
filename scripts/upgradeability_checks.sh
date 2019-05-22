e059d1f99805b9cec6f714b1dc18fb0c6a866bb2
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2019-05-22 15:18:46 +0200

    update travis pipeline

--- scripts/upgradeability_checks.sh
+++ scripts/upgradeability_checks.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # THIS SCRIPT USES SLITHER TO VALIDATE THAT THE CURRENT KEEPER CONTRACTS ARE UPGRADEABLE
-# for more info about Slither contract upgradeability checks, check out the link below
+# for more info about Slither contract upgradeability check, check out the link below
 # https://github.com/crytic/slither/wiki/Upgradeability-Checks#proxy-contract
+
 # Setup zos lib
 git clone https://github.com/zeppelinos/zos
 cd zos/packages/lib
 npm install
 rm contracts/mocks/WithConstructorImplementation.sol
 cd ../../../
+
 # run slither-check-upgradeability
 files=$(pwd)/files
 for entry in contracts/*/*/*
 do
-    echo $(basename "$entry") | cut -f 1 -d "." >> files
+    echo `basename "$entry"` | cut -f 1 -d "." >> files
 done
 while IFS= read -r line
 do
