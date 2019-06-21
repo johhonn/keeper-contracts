31015be91d943c6561a29af8c3846ac148d08aa8
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2019-06-21 10:40:50 +0200

    document condition.sol

--- contracts/conditions/Condition.sol
+++ contracts/conditions/Condition.sol
@@ -8,14 +8,26 @@ import 'openzeppelin-eth/contracts/ownership/Ownable.sol';
  *
  * @dev Implementation of the Condition
  *
- *      
- *      For more information: https://github.com/oceanprotocol/OEPs/issues/133
- *      TODO: update the OEP link 
+ *      Each condition has a validation function that returns either FULFILLED, 
+ *      ABORTED or UNFULFILLED. When a condition is successfully solved, we call 
+ *      it FULFILLED. If a condition cannot be FULFILLED anymore due to a timeout 
+ *      or other types of counter-proofs, the condition is ABORTED. UNFULFILLED 
+ *      values imply that a condition has not been provably FULFILLED or ABORTED. 
+ *      All initialized conditions start out as UNFULFILLED.
+ *      For more information please refer to this link: 
+ *      https://github.com/oceanprotocol/OEPs/issues/133
+ *      TODO: update the OEP link
  */
 contract Condition is Ownable {
 
     ConditionStoreManager internal conditionStoreManager;
 
+   /**
+    * @notice generateId condition Id from the following 
+    *       parameters
+    * @param _agreementId SEA agreement ID
+    * @param _valueHash hash of all the condition input values
+    */
     function generateId(
         bytes32 _agreementId,
         bytes32 _valueHash
@@ -33,6 +45,12 @@ contract Condition is Ownable {
         );
     }
 
+   /**
+    * @notice fulfill set the condition state to Fulfill | Abort
+    * @param _id condition identifier
+    * @param _newState new condition state (Fulfill/Abort)
+    * @return the updated condition state 
+    */
     function fulfill(
         bytes32 _id,
         ConditionStoreLibrary.ConditionState _newState
@@ -45,6 +63,12 @@ contract Condition is Ownable {
     }
 
 
+    /**
+    * @notice abortByTimeOut set condition state to Aborted 
+    *         if the condition is timed out
+    * @param _id condition identifier
+    * @return the updated condition state
+    */
     function abortByTimeOut(
         bytes32 _id
     )
