89b783301b95474ff79fa4cb48f1bcd608e95b69
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2019-06-24 16:14:18 +0200

    update the template documentation

--- contracts/templates/TemplateStoreManager.sol
+++ contracts/templates/TemplateStoreManager.sol
@@ -3,6 +3,20 @@ pragma solidity 0.5.6;
 import './TemplateStoreLibrary.sol';
 import 'openzeppelin-eth/contracts/ownership/Ownable.sol';
 
+/**
+ * @title Template Store Manager
+ * @author Ocean Protocol Team
+ *
+ * @dev Implementation of the Template Store Manager.
+ *      Templates are blueprints for modular SEAs. When creating an Agreement, 
+ *      a templateId defines the condition and reward types that are instantiated 
+ *      in the ConditionStore. This contract manages the life cycle 
+ *      of the template ( Propose --> Approve --> Revoke ).
+ *      For more information please refer to this link:
+ *      https://github.com/oceanprotocol/OEPs/issues/132
+ *      TODO: link to OEP
+ *      
+ */
 contract TemplateStoreManager is Ownable {
 
     using TemplateStoreLibrary for TemplateStoreLibrary.TemplateList;
@@ -18,6 +32,11 @@ contract TemplateStoreManager is Ownable {
         _;
     }
 
+    /**
+     * @dev initialize TemplateStoreManager Initializer
+     *      Initializes Ownable. Only on contract creation.
+     * @param _owner refers to the owner of the contract
+     */
     function initialize(
         address _owner
     )
@@ -32,6 +51,11 @@ contract TemplateStoreManager is Ownable {
         Ownable.initialize(_owner);
     }
 
+    /**
+     * @notice proposeTemplate proposes a new template
+     * @param _id unique template identifier which is basically
+     *        the template contract address
+     */
     function proposeTemplate(address _id)
         external
         returns (uint size)
@@ -39,6 +63,12 @@ contract TemplateStoreManager is Ownable {
         return templateList.propose(_id);
     }
 
+    /**
+     * @notice approveTemplate approves a template
+     * @param _id unique template identifier which is basically
+     *        the template contract address. Only template store
+     *        manager owner (i.e OPNF) can approve this template.
+     */
     function approveTemplate(address _id)
         external
         onlyOwner
@@ -46,6 +76,13 @@ contract TemplateStoreManager is Ownable {
         return templateList.approve(_id);
     }
 
+    /**
+     * @notice revokeTemplate revoke a template
+     * @param _id unique template identifier which is basically
+     *        the template contract address. Only template store
+     *        manager owner (i.e OPNF) or template owner
+     *        can revoke this template.
+     */
     function revokeTemplate(address _id)
         external
         onlyOwnerOrTemplateOwner(_id)
@@ -53,6 +90,13 @@ contract TemplateStoreManager is Ownable {
         return templateList.revoke(_id);
     }
 
+    /**
+     * @notice getTemplate get more information about a template
+     * @param _id unique template identifier which is basically
+     *        the template contract address.
+     * @return template status, template owner, last updated by and
+     *        last updated at.
+     */
     function getTemplate(address _id)
         external
         view
@@ -69,6 +113,10 @@ contract TemplateStoreManager is Ownable {
         blockNumberUpdated = templateList.templates[_id].blockNumberUpdated;
     }
 
+    /**
+     * @notice getTemplateListSize number of templates
+     * @return number of templates
+     */
     function getTemplateListSize()
         external
         view
@@ -77,6 +125,12 @@ contract TemplateStoreManager is Ownable {
         return templateList.templateIds.length;
     }
 
+    /**
+     * @notice isTemplateApproved check whether the template is approved
+     * @param _id unique template identifier which is basically
+     *        the template contract address.
+     * @return true if the template is approved
+     */
     function isTemplateApproved(address _id) external view returns (bool) {
         return templateList.templates[_id].state ==
             TemplateStoreLibrary.TemplateState.Approved;
