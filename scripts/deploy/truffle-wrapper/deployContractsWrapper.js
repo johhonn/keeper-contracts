327168db91c328c9f457e626de56225349e2fc18
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-06-25 15:11:49 +0200

    moved setupContracts and evaluteContracts back from dori because they are project specific.

--- scripts/deploy/truffle-wrapper/deployContractsWrapper.js
+++ scripts/deploy/truffle-wrapper/deployContractsWrapper.js
@@ -1,7 +1,9 @@
 /* global artifacts, web3 */
 const { argv } = require('yargs')
 const { deployContracts } = require('@oceanprotocol/dori')
-const initializeContracts = require('./initializeContracts.js')
+const initializeContracts = require('./deploy/initializeContracts.js')
+const setupContracts = require('./deploy/setupContracts.js')
+const evaluateContracts = require('./evaluateContracts.js')
 
 module.exports = (cb) => {
     const parameters = argv._
@@ -11,7 +13,9 @@ module.exports = (cb) => {
         web3,
         artifacts,
         contracts,
+        evaluateContracts,
         initializeContracts,
+        setupContracts,
         forceWalletCreation: argv['force-wallet-creation'] || false,
         deeperClean: argv['deeper-clean'] || false,
         testnet: argv['testnet'] || false,
---
+++ scripts/deploy/truffle-wrapper/evaluateContracts.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+// List of contracts
+const contractNames = require(`${process.env.PWD}/contracts.json`)
+
+function evaluateContracts({
+    contracts,
+    testnet,
+    verbose
+}) {
+    if (!contracts || contracts.length === 0) {
+        // contracts not supplied, loading from disc
+        contracts = contractNames
+
+        // if we are on a testnet, add dispenser
+        if (testnet && contracts.indexOf('Dispenser') < 0) {
+            // deploy the Dispenser if we are in a testnet
+            contracts.push('Dispenser')
+        }
+    }
+
+    if (verbose) {
+        console.log(
+            `Contracts: '${contracts.join(', ')}'`
+        )
+    }
+
+    return contracts
+}
+
+module.exports = evaluateContracts
