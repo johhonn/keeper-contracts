4a91ba927bee4576fa0bcb53cb6291d6b970d0c7
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-06-27 10:10:20 +0200

    add proper padding to the output of the addresses, also added the tables head

--- scripts/contracts/get-addresses.js
+++ scripts/contracts/get-addresses.js
@@ -1,30 +1,38 @@
 const fs = require('fs')
-
-const root = `${__dirname}/../..`
-const pkg = require(`${root}/package.json`)
-const { version } = pkg
 const network = process.argv[2]
 
 // The location of the artifact files.
-const buildDirectory = `${root}/artifacts/`
+const buildDirectory = `${process.env.PWD}/artifacts/`
 
 // Loop through all of the files.
+// eslint-disable-next-line security/detect-non-literal-fs-filename
 fs.readdir(buildDirectory, (err, files) => {
     if (err) {
         throw err
     }
 
-    files.forEach((filename) => {
-        if (!filename.includes(`.${network}.`)) {
-            return
-        }
+    const artifacts = files.filter((f) => f.includes(`.${network}.`))
 
-        const artifactString = fs.readFileSync(`${buildDirectory}${filename}`, 'utf8')
+    if (artifacts.length < 1) {
+        return
+    }
+
+    // eslint-disable-next-line no-console
+    console.log(`| ${'Contract'.padEnd(33)} | Version | ${'Address'.padEnd(44)} |`)
+
+    // eslint-disable-next-line no-console
+    console.log(`|-----------------------------------|---------|----------------------------------------------|`)
+
+    artifacts.forEach((filename) => {
+        const artifactString = fs.readFileSync(
+            `${buildDirectory}${filename}`,
+            'utf8'
+        )
         const artifact = JSON.parse(artifactString)
 
         const contractName = filename.split('.')[0]
 
         // eslint-disable-next-line no-console
-        console.log(`| ${contractName} | v${version} | ${artifact.address} |`)
+        console.log(`| ${contractName.padEnd(33)} | ${artifact.version} | \`${artifact.address}\` |`)
     })
 })
