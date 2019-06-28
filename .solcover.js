5093cd318cb03daf49a99199ff0ca7658c1ae556
Sebastian Gerske <sebastian.gerske@gmail.com>
2019-06-28 10:30:15 +0200

    revert changes, use sol cover ganache again

--- .solcover.js
+++ .solcover.js
@@ -1,9 +1,9 @@
 module.exports = {
-    norpc: true,
-    port: 8545,
     compileCommand: 'npm run compile -- --all',
-    testCommand: 'export ETHEREUM_RPC_PORT=8545&& npm run test:fast -- --network coverage --timeout 10000',
-    copyPackages: ['openzeppelin-eth'],
+    testCommand: 'export ETHEREUM_RPC_PORT=8555&& npm run test:fast -- --network coverage --timeout 10000',
+    copyPackages: [
+        'openzeppelin-eth'
+    ],
     skipFiles: [
         'test'
     ],
