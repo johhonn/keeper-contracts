b83835531d319d7c0e6016a8e2d0ad24e9792320
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2019-07-22 10:54:34 +0200

    add documentation

--- contracts/conditions/ThresholdCondition.sol
+++ contracts/conditions/ThresholdCondition.sol
@@ -5,7 +5,29 @@ pragma solidity 0.5.6;
 
 import './Condition.sol';
 
+/**
+ * @title Threshold Condition
+ * @author Ocean Protocol Team
+ *
+ * @dev Implementation of the Threshold Condition
+ *
+ *      
+ *      Threshold condition acts as a filter for a set of input condition(s) in which sends 
+ *      a signal whether to complete the flow execution or abort it. This type of conditions 
+ *      works as intermediary conditions where they wire SEA conditions in order to support  
+ *      more complex scenarios. The logic gate conditions can be categorized into three basic
+ *      condition types but also we can expend this to support more features in the future.
+ */
 contract ThresholdCondition is Condition {
+    
+   /**
+    * @notice initialize init the 
+    *       contract with the following parameters
+    * @dev this function is called only once during the contract
+    *       initialization.
+    * @param _owner contract's owner account address
+    * @param _conditionStoreManagerAddress condition store manager address
+    */
     function initialize(
         address _owner,
         address _conditionStoreManagerAddress
@@ -23,6 +45,13 @@ contract ThresholdCondition is Condition {
         );
     }
 
+   /**
+    * @notice hashValues generates the hash of condition inputs 
+    *        with the following parameters
+    * @param inputConditions array of input conditions IDs
+    * @param nInputFulfilledConditions the required number of fulfilled input conditions
+    * @return bytes32 hash of all these values 
+    */
     function hashValues(
         bytes32[] memory inputConditions, 
         uint256 nInputFulfilledConditions    
@@ -39,7 +68,15 @@ contract ThresholdCondition is Condition {
         );
     }
     
-    
+   /**
+    * @notice fulfill threshold condition
+    * @dev the fulfill method check whether input conditions are
+    *       fulfilled or not.
+    * @param _agreementId agreement identifier
+    * @param _inputConditions array of input conditions IDs
+    * @param nInputFulfilledConditions the required number of fulfilled input conditions
+    * @return condition state (Fulfilled/Aborted)
+    */
     function fulfill(
         bytes32 _agreementId,
         bytes32[] calldata _inputConditions,
@@ -71,7 +108,12 @@ contract ThresholdCondition is Condition {
         );
     }
     
-    
+   /**
+    * @notice canFulfill check if condition can be fulfilled
+    * @param _inputConditions array of input conditions IDs
+    * @param nInputFulfilledConditions the required number of fulfilled input conditions
+    * @return true if can fulfill 
+    */
     function canFulfill(
         bytes32[] memory _inputConditions,
         uint256 nInputFulfilledConditions
