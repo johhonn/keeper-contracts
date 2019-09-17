0d3e595212cec9bd137b4603ba998a097877fdaa
Ahmed Abdullah <ahmedaabdulwahed@gmail.com>
2019-09-17 14:59:28 +0200

    fix all test cases

--- test/unit/templates/TemplateStoreManager.Test.js
+++ test/unit/templates/TemplateStoreManager.Test.js
@@ -10,6 +10,9 @@ chai.use(chaiAsPromised)
 const Common = artifacts.require('Common')
 const TemplateStoreLibrary = artifacts.require('TemplateStoreLibrary')
 const TemplateStoreManager = artifacts.require('TemplateStoreManager')
+const HashLockCondition = artifacts.require('HashLockCondition')
+const ConditionStoreManager = artifacts.require('ConditionStoreManager')
+const EpochLibrary = artifacts.require('EpochLibrary')
 
 const constants = require('../../helpers/constants.js')
 
@@ -25,12 +28,52 @@ contract('TemplateStoreManager', (accounts) => {
         const templateStoreManager = await TemplateStoreManager.new()
         await templateStoreManager.initialize(createRole)
 
+        const epochLibrary = await EpochLibrary.new()
+        await ConditionStoreManager.link('EpochLibrary', epochLibrary.address)
+        const owner = createRole
+        const conditionStoreManager = await ConditionStoreManager.new()
+        await conditionStoreManager.initialize(
+            owner,
+            { from: owner }
+        )
+
+        await conditionStoreManager.delegateCreateRole(
+            createRole,
+            { from: owner }
+        )
+        const hashLockCondition = await HashLockCondition.new()
+        await hashLockCondition.initialize(
+            owner,
+            conditionStoreManager.address,
+            { from: owner }
+        )
+
+        await templateStoreManager.registerTemplateActorType(
+            'consumer',
+            {
+            from: owner
+            }
+        )
+        const consumerActorTypeId = await templateStoreManager.getTemplateActorTypeId('consumer')
+
+        // any random ID
+        templateId = constants.bytes32.one
+
+        conditionTypes = [
+            hashLockCondition.address
+        ]
+        actorTypeIds = [
+            consumerActorTypeId
+        ]
         return {
             common,
             templateStoreManager,
             templateId,
             conditionType,
-            createRole
+            createRole,
+            conditionTypes,
+            actorTypeIds,
+            owner
         }
     }
 
@@ -45,11 +88,17 @@ contract('TemplateStoreManager', (accounts) => {
 
     describe('propose template', () => {
         it('should propose and be proposed', async () => {
-            const { templateStoreManager } = await setupTest()
+            const { templateStoreManager, conditionTypes, templateId, actorTypes, owner } = await setupTest()
 
-            const templateId = constants.address.dummy
-
-            await templateStoreManager.proposeTemplate(templateId)
+            await templateStoreManager.methods['proposeTemplate(bytes32,address[],bytes32[],string)'](
+                templateId,
+                conditionTypes,
+                actorTypeIds,
+                'SampleTemplate',
+                {
+                    from: owner
+                }
+            )
 
             expect((await templateStoreManager.getTemplate(templateId)).state.toNumber())
                 .to.equal(constants.template.state.proposed)
@@ -57,14 +106,28 @@ contract('TemplateStoreManager', (accounts) => {
         })
 
         it('should not propose if exists', async () => {
-            const { templateStoreManager } = await setupTest()
+            const { templateStoreManager, templateId, conditionTypes, actorTypeIds, owner } = await setupTest()
 
-            const templateId = constants.address.dummy
-
-            await templateStoreManager.proposeTemplate(templateId)
+            await templateStoreManager.methods['proposeTemplate(bytes32,address[],bytes32[],string)'](
+                templateId,
+                conditionTypes,
+                actorTypeIds,
+                'SampleTemplate',
+                {
+                    from: owner
+                }
+            )
 
             await assert.isRejected(
-                templateStoreManager.proposeTemplate(templateId),
+                templateStoreManager.methods['proposeTemplate(bytes32,address[],bytes32[],string)'](
+                    templateId,
+                    conditionTypes,
+                    actorTypeIds,
+                    'SampleTemplate',
+                    {
+                        from: owner
+                    }
+                ),
                 constants.error.idAlreadyExists
             )
         })
@@ -72,28 +135,41 @@ contract('TemplateStoreManager', (accounts) => {
 
     describe('approve template', () => {
         it('should approve after propose', async () => {
-            const { templateStoreManager } = await setupTest()
-
-            const templateId = constants.address.dummy
+            const { templateStoreManager, templateId, conditionTypes, actorTypeIds, owner } = await setupTest()
 
             await assert.isRejected(
                 templateStoreManager.approveTemplate(templateId),
                 constants.template.error.templateNotProposed
             )
 
-            await templateStoreManager.proposeTemplate(templateId)
-            await templateStoreManager.approveTemplate(templateId)
+            await templateStoreManager.methods['proposeTemplate(bytes32,address[],bytes32[],string)'](
+                templateId,
+                conditionTypes,
+                actorTypeIds,
+                'SampleTemplate',
+                {
+                    from: owner
+                }
+            )
+            await templateStoreManager.approveTemplate(templateId, { from: owner })
             expect((await templateStoreManager.getTemplate(templateId)).state.toNumber())
                 .to.equal(constants.template.state.approved)
             expect((await templateStoreManager.getTemplateListSize()).toNumber()).to.equal(1)
         })
 
         it('should not approve if not createRole', async () => {
-            const { templateStoreManager } = await setupTest()
+            const { templateStoreManager, templateId, actorTypeIds, conditionTypes, owner } = await setupTest()
 
-            const templateId = constants.address.dummy
+            await templateStoreManager.methods['proposeTemplate(bytes32,address[],bytes32[],string)'](
+                templateId,
+                conditionTypes,
+                actorTypeIds,
+                'SampleTemplate',
+                {
+                    from: owner
+                }
+            )
 
-            await templateStoreManager.proposeTemplate(templateId)
             await assert.isRejected(
                 templateStoreManager.approveTemplate(templateId, { from: accounts[1] }),
                 constants.error.revert
@@ -103,12 +179,18 @@ contract('TemplateStoreManager', (accounts) => {
 
     describe('get template', () => {
         it('successful create should get unfulfilled condition', async () => {
-            const { common, templateStoreManager } = await setupTest()
-
+            const { common, templateStoreManager, templateId, conditionTypes, actorTypeIds, owner } = await setupTest()
             const blockNumber = await common.getCurrentBlockNumber()
-            const templateId = constants.address.dummy
 
-            await templateStoreManager.proposeTemplate(templateId)
+            await templateStoreManager.methods['proposeTemplate(bytes32,address[],bytes32[],string)'](
+                templateId,
+                conditionTypes,
+                actorTypeIds,
+                'SampleTemplate',
+                {
+                    from: owner
+                }
+            )
 
             // TODO - containSubset
             const storedTemplate = await templateStoreManager.getTemplate(templateId)
@@ -123,16 +205,20 @@ contract('TemplateStoreManager', (accounts) => {
 
     describe('revoke template', () => {
         it('successful create should revoke if owner and approved', async () => {
-            const { common, templateStoreManager } = await setupTest()
+            const { common, templateStoreManager, templateId, conditionTypes, actorTypeIds, owner } = await setupTest()
 
-            const templateId = constants.address.dummy
+            await templateStoreManager.methods['proposeTemplate(bytes32,address[],bytes32[],string)'](
+                templateId,
+                conditionTypes,
+                actorTypeIds,
+                'SampleTemplate'
+            )
 
-            await templateStoreManager.proposeTemplate(templateId)
-            await templateStoreManager.approveTemplate(templateId)
+            await templateStoreManager.approveTemplate(templateId, { from: owner })
 
             const blockNumber = await common.getCurrentBlockNumber()
 
-            await templateStoreManager.revokeTemplate(templateId)
+            await templateStoreManager.revokeTemplate(templateId, { from: owner })
 
             const storedTemplate = await templateStoreManager.getTemplate(templateId)
             expect(storedTemplate.state.toNumber())
@@ -142,54 +228,72 @@ contract('TemplateStoreManager', (accounts) => {
         })
 
         it('successful approve should not revoke if not owner', async () => {
-            const { templateStoreManager } = await setupTest()
+            const { templateStoreManager, templateId, actorTypeIds, conditionTypes, owner } = await setupTest()
 
-            const templateId = constants.address.dummy
+            await templateStoreManager.methods['proposeTemplate(bytes32,address[],bytes32[],string)'](
+                templateId,
+                conditionTypes,
+                actorTypeIds,
+                'SampleTemplate',
+                {
+                    from: owner
+                }
+            )
 
-            await templateStoreManager.proposeTemplate(templateId)
-            await templateStoreManager.approveTemplate(templateId)
+            await templateStoreManager.approveTemplate(templateId, { from: owner })
 
             await assert.isRejected(
                 templateStoreManager.revokeTemplate(templateId, { from: accounts[1] }),
-                constants.acl.error.invalidUpdateRole
+                'Invalid contract owner or template owner'
             )
         })
 
         it('should not revoke if uninitialized', async () => {
-            const { templateStoreManager } = await setupTest()
-
-            const templateId = constants.address.dummy
+            const { templateStoreManager, templateId, owner } = await setupTest()
 
             await assert.isRejected(
-                templateStoreManager.revokeTemplate(templateId),
+                templateStoreManager.revokeTemplate(templateId, { from: owner }),
                 constants.template.error.templateNotApproved
             )
         })
 
         it('should not revoke if proposed', async () => {
-            const { templateStoreManager } = await setupTest()
+            const { templateStoreManager, templateId, actorTypeIds, conditionTypes, owner } = await setupTest()
 
-            const templateId = constants.address.dummy
-
-            await templateStoreManager.proposeTemplate(templateId)
+            await templateStoreManager.methods['proposeTemplate(bytes32,address[],bytes32[],string)'](
+                templateId,
+                conditionTypes,
+                actorTypeIds,
+                'SampleTemplate',
+                {
+                    from: owner
+                }
+            )
 
             await assert.isRejected(
-                templateStoreManager.revokeTemplate(templateId),
+                templateStoreManager.revokeTemplate(templateId, { from: owner }),
                 constants.template.error.templateNotApproved
             )
         })
 
         it('should not revoke if already revoked', async () => {
-            const { templateStoreManager } = await setupTest()
+            const { templateStoreManager, actorTypeIds, conditionTypes, templateId, owner } = await setupTest()
 
-            const templateId = constants.address.dummy
+            await templateStoreManager.methods['proposeTemplate(bytes32,address[],bytes32[],string)'](
+                templateId,
+                conditionTypes,
+                actorTypeIds,
+                'SampleTemplate',
+                {
+                    from: owner
+                }
+            )
 
-            await templateStoreManager.proposeTemplate(templateId)
-            await templateStoreManager.approveTemplate(templateId)
-            await templateStoreManager.revokeTemplate(templateId)
+            await templateStoreManager.approveTemplate(templateId, { from: owner })
+            await templateStoreManager.revokeTemplate(templateId, { from: owner })
 
             await assert.isRejected(
-                templateStoreManager.revokeTemplate(templateId),
+                templateStoreManager.revokeTemplate(templateId, { from: owner }),
                 constants.template.error.templateNotApproved
             )
         })
